fe0865e88aea5c1c4ed912ea94609ff9ad396dcd
mac <mac@macui-MacBook-Air.local>
2023-02-07 23:08:17 +0900

    투두리스트 구현

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -8,18 +8,16 @@ interface TodoListProps {
 export default function TodoList(props: TodoListProps) {
   return (
     <>
+      <h1>할 일 </h1>
       <ul>
         {props.todos.map((todo) => {
           return (
-            <>
-              <h1>할 일 </h1>
-              <li key={todo.id}>
-                <span>{todo.text}</span>
-                <button onClick={props.deleteTodo.bind(null, todo.id)}>
-                  삭제
-                </button>
-              </li>
-            </>
+            <li key={todo.id}>
+              <span key={todo.id}>{todo.text}</span>
+              <button onClick={props.deleteTodo.bind(null, todo.id)}>
+                삭제
+              </button>
+            </li>
           );
         })}
       </ul>
